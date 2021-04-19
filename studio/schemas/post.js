681970f5abcce09418dacbd274139d868f8d603e
eslamheshamm <esl2mhesham@gmail.com>
2021-04-19 22:57:22 +0200

    blog schemas

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -1,65 +1,66 @@
 export default {
-  name: 'post',
-  title: 'Post',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    },
-    {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    },
-    {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    },
-  ],
+	name: "post",
+	title: "Post",
+	type: "document",
+	fields: [
+		{
+			name: "title",
+			title: "Title",
+			type: "string",
+		},
+		{
+			name: "slug",
+			title: "Slug",
+			type: "slug",
+			options: {
+				source: "title",
+				maxLength: 96,
+			},
+		},
 
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      })
-    },
-  },
-}
+		{
+			name: "mainImage",
+			title: "Main image",
+			type: "image",
+			options: {
+				hotspot: true,
+			},
+		},
+		{
+			name: "excerpt",
+			type: "text",
+			title: "Summery",
+			validation: (Rule) => Rule.required().min(10).max(150),
+		},
+		{
+			name: "categories",
+			title: "Categories",
+			type: "array",
+			of: [{ type: "reference", to: { type: "category" } }],
+		},
+		{
+			name: "publishedAt",
+			title: "Published at",
+			type: "datetime",
+		},
+		{
+			name: "body",
+			title: "Body",
+			type: "blockContent",
+		},
+	],
+
+	preview: {
+		select: {
+			title: "title",
+			author: "author.name",
+			media: "mainImage",
+		},
+		prepare(selection) {
+			const { author } = selection;
+			return Object.assign({}, selection, {
+				subtitle: author && `by ${author}`,
+			});
+		},
+	},
+};
